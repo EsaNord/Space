f294be4c582710890b98cc31c5ec49b6089cd217
AntoniusMaximus80 <toni.mutanen@cs.tamk.fi>
2017-06-08 11:04:44 +0300

    no message

---
+++ Assets/Crate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : MonoBehaviour {
+
+    private Rigidbody2D rigid;
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        audio = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(rigid.velocity.x > 2f || rigid.velocity.y > 2f)
+        {
+            audio.Play();
+        }
+    }
+}
