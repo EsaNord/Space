440077c59ca7b12a68f5272d59fab0d535a3c8ee
Esa Nord <esa.nord@cs.tamk.fi>
2017-06-09 09:46:28 +0300

    Ending added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
@@ -178,5 +179,11 @@ public class Player : MonoBehaviour {
 				}
 			}
 		}
-	}	
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "End") {
+			SceneManager.LoadScene ("Menu");
+		}
+	}
 }
